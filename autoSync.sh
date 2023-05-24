40440f87927af743979c2e4ee5cbd73445bbaabf
shengguangbiao.1703 <shengguangbiao.1703@bytedance.com>
2023-05-24 09:41:13 +0800

    add some code from Mac

--- autoSync.sh
+++ autoSync.sh
@@ -9,4 +9,4 @@ git commit -m 'add some code from Mac'
 # git commit -m 'add some results from Server'
 git push             #upload data
 
-ssh root@42.192.3.108 "cd /var/www/blogs; git pull"
+ssh root@42.192.3.108 "cd /var/www/blogs; git reset --hard origin/main"
